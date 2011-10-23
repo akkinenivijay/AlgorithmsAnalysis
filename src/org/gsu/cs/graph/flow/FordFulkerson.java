2e3898aae1db6a53ac1a3f68a36598044848c2cd
akkinenivijay <akkineni.vijay@gmail.com>
2011-10-23 19:25:56 -0400

    Added main execute method ..meed to check boundary conditions and graph consistency.

--- src/org/gsu/cs/graph/flow/FordFulkerson.java
+++ src/org/gsu/cs/graph/flow/FordFulkerson.java
@@ -10,12 +10,26 @@ public class FordFulkerson {
 
 	private boolean[] marked;
 	private Edge[] edgeTo;
+	private double value;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		AdjacencyListGraph G = new AdjacencyListGraph(4, 6, "");
+		System.out.println(G);
+		FordFulkerson ff = new FordFulkerson();
+		ff.execute(G, 0, 3);
+		System.out.println("Max flow from " + 0 + " to " + 3);
+
+		for (int v = 0; v < G.getVertices(); v++) {
+			for (Edge e : G.adjacentList(v)) {
+				if ((v == e.getFrom()) && e.getFlow() > 0)
+					System.out.println("   " + e);
+			}
+		}
+
+		System.out.println("Max flow value = " + ff.value);
 
 	}
 
@@ -43,6 +57,43 @@ public class FordFulkerson {
 		}
 
 		return marked[t];
+	}
+
+	public void execute(AdjacencyListGraph G, int s, int t) {
+		value = excess(G, t);
+		// if (!isFeasible(G, s, t)) {
+		// throw new RuntimeException("Initial flow is infeasible");
+		// }
+
+		// while there exists an augmenting path, use it
+		while (isAugmentingPathPresent(G, s, t)) {
+
+			// compute bottleneck capacity
+			int bottle = Integer.MAX_VALUE;
+			for (int v = t; v != s; v = edgeTo[v].other(v)) {
+				bottle = Math.min(bottle, edgeTo[v].residualCapacityTo(v));
+			}
+
+			// augment flow
+			for (int v = t; v != s; v = edgeTo[v].other(v)) {
+				edgeTo[v].addResidualFlowTo(v, bottle);
+			}
+
+			value += bottle;
+		}
+
+	}
+
+	// return excess flow at vertex v
+	private int excess(AdjacencyListGraph G, int u) {
+		int excess = 0;
+		for (Edge e : G.adjacentList(u)) {
+			if (u == e.getFrom())
+				excess -= e.getFlow();
+			else
+				excess += e.getFlow();
+		}
+		return excess;
 	}
 
 }
